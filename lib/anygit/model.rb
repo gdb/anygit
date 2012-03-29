5e9cb3c88a7d941478269ce627eae9034182bf21
Greg Brockman <gdb@gregbrockman.com>
2012-03-29 14:11:30 -0700

    Move to simpler world

--- lib/anygit/model.rb
+++ lib/anygit/model.rb
@@ -1,4 +1,5 @@
 require 'data_mapper'
+require 'cgi'
 
 module Anygit
   module Model
@@ -27,8 +28,25 @@ module Anygit
       property :id, Serial
       # TODO: support aliasing
       property :url, String, :length => 3000
+      property :template, String, :length => 3000
       property :created_at, DateTime
       property :fetched_at, DateTime
+
+      # Really should store the template
+      def webview(type, hex_sha1)
+        return template.gsub('{sha1}', hex_sha1) if template
+
+        parsed = URI.parse(url)
+        if parsed.host == 'github.com'
+          parsed
+          parsed.scheme = 'https'
+          parsed.path = $1 if parsed.path =~ /^(.*)(\.git|\/)$/
+          parsed.path += "/#{CGI.escape(type)}/#{CGI.escape(hex_sha1)}"
+        else
+          raise "Not sure how to build a template for #{self}: #{parsed}"
+        end
+        parsed.to_s
+      end
     end
 
     class GitObject
@@ -52,17 +70,17 @@ module Anygit
       property :created_at, DateTime
     end
 
-    class ObjectPointer
-      include DataMapper::Resource
+    # class ObjectPointer
+    #   include DataMapper::Resource
 
-      # Requires my patches to dm-core / dm-migrations.
-      #
-      # Note that index isn't autocreating itself at the moment (might
-      # want to reverse order?) so run
-      # CREATE INDEX target ON anygit_model_object_pointers (target ASC)
-      property :source, String, SHA1_KEY_OPTS
-      property :target, String, SHA1_KEY_OPTS.merge(:index => true)
-      property :created_at, DateTime
-    end
+    #   # Requires my patches to dm-core / dm-migrations.
+    #   #
+    #   # Note that index isn't autocreating itself at the moment (might
+    #   # want to reverse order?) so run
+    #   # CREATE INDEX target ON anygit_model_object_pointers (target ASC)
+    #   property :source, String, SHA1_KEY_OPTS
+    #   property :target, String, SHA1_KEY_OPTS.merge(:index => true)
+    #   property :created_at, DateTime
+    # end
   end
 end
--- lib/anygit/web_interface.rb
+++ lib/anygit/web_interface.rb
@@ -4,10 +4,18 @@ require 'rack/utils'
 
 module Anygit
   class WebInterface < Sinatra::Base
+    LIMIT = 10
     set :root, Pathname.new(File.join(File.dirname(__FILE__), '../..')).realpath
 
+    get '/q/?' do
+      @limit = LIMIT
+      @collection = Model::GitObject.all(:limit => @limit)
+      @sha1 = nil
+      erb :q_many
+    end
+
     get '/q/:sha1' do
-      @limit = 10
+      @limit = LIMIT
       @sha1 = sha1 = params[:sha1]
 
       if sha1.length > 40
@@ -26,25 +34,35 @@ module Anygit
       end
 
       if @collection.count == 1
+        # Should start using joins
         object = @collection.first
-
-        op_name = Util.validate_table_name(Anygit::Model::ObjectPointer.storage_name)
-        go_name = Util.validate_table_name(Anygit::Model::GitObject.storage_name)
-        # TODO: paginate, filter by type
-        @raw_pointers = repository(:default).adapter.select("
-SELECT b.sha1, b.type
-FROM #{op_name} AS a LEFT JOIN #{go_name} AS b
-ON a.source = b.sha1
-WHERE a.target = ?
-LIMIT ?
-", object.sha1, @limit)
-        @git_objects = @raw_pointers.map do |pointer|
-          go = Model::GitObject.new
-          go.sha1 = pointer.sha1
-          go.type = pointer.type
-          go
+        unless object_repo = Model::ObjectRepo.first(:sha1 => object.sha1)
+          raise "Data corruption: objectrepo found"
         end
-        erb :q_one
+        repo = object_repo.repo
+        dest = repo.webview(object.type, object.hex_sha1)
+        Anygit.log.info("Redirecting to #{dest}")
+        redirect(dest)
+
+# Might be useful if we start indexing all the arrows
+#
+#         op_name = Util.validate_table_name(Anygit::Model::ObjectPointer.storage_name)
+#         go_name = Util.validate_table_name(Anygit::Model::GitObject.storage_name)
+#         # TODO: paginate, filter by type
+#         @raw_pointers = repository(:default).adapter.select("
+# SELECT b.sha1, b.type
+# FROM #{op_name} AS a LEFT JOIN #{go_name} AS b
+# ON a.source = b.sha1
+# WHERE a.target = ?
+# LIMIT ?
+# ", object.sha1, @limit)
+#         @git_objects = @raw_pointers.map do |pointer|
+#           go = Model::GitObject.new
+#           go.sha1 = pointer.sha1
+#           go.type = pointer.type
+#           go
+#         end
+#        erb :q_one
       else
         erb :q_many
       end
